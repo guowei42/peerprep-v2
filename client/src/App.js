47bea5422191c90d5229b3305ec748c7fda94e35
Guo Wei <65711401+guowei42@users.noreply.github.com>
2024-10-10 14:00:29 +0800

    Temp home page layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
-
-
+import { Container } from "@mui/material";
 
 function App() {
-
   return (
     <div className="App">
-      <Header/>
-      <Outlet/>
+      <Header />
+      <Container className="outlet">
+        <Outlet />
+      </Container>
     </div>
   );
 }
