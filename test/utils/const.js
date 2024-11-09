3ff73178b40fcc15f26896a59db6c2772b649cbf
Xenos F. <git@xenosfio.com>
2024-11-10 04:16:19 +0800

    Change 127.0.0.1 -> localhost

--- test/utils/const.js
+++ test/utils/const.js
@@ -1,6 +1,6 @@
 module.exports.ROOT_URL = "http://localhost:3000";
-module.exports.USERS_API_URL = "http://127.0.0.1:3001";
-module.exports.QUESTIONS_API_URL = "http://127.0.0.1:3002";
+module.exports.USERS_API_URL = "http://localhost:3001";
+module.exports.QUESTIONS_API_URL = "http://localhost:3002";
 
 module.exports.TIMEOUT = 30 * 1000;
 
