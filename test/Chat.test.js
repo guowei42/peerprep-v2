0c5acaefaf712a35fc54a6a31ec0d379dd0d4d46
Xenos F. <git@xenosfio.com>
2024-11-13 13:58:29 +0800

    Add chat test

---
+++ test/Chat.test.js
@@ -0,0 +1,46 @@
+let {
+  getWebDriver,
+  findButtonContainingText,
+  findTextInputWithLabel,
+  fillTextInput,
+  click,
+} = require("./utils/driver");
+let { TEST_QUESTION, TIMEOUT_MATCHING_TESTS } = require("./utils/const");
+const { By, until } = require("selenium-webdriver");
+const { startSession, setupMatchingTests, resetServer } = require("./utils/utils");
+
+describe("Chat tests", () => {
+  let driver1, driver2;
+
+  beforeAll(async () => {
+    driver1 = await getWebDriver();
+    driver2 = await getWebDriver();
+    await resetServer();
+  });
+
+  afterAll(async () => {
+    if (driver1) await driver1.quit();
+    if (driver2) await driver2.quit();
+  });
+
+  beforeEach(async () => {
+    await setupMatchingTests(driver1, driver2);
+    await startSession(driver1, driver2, TEST_QUESTION.complexity, TEST_QUESTION.categories);
+  }, TIMEOUT_MATCHING_TESTS);
+
+  test("chat message should show correctly for both sender and partner", async () => {
+    let chatTextBoxLabel = "Type your message";
+    let msg = "hello from 1 :)";
+    let chatInput1 = await findTextInputWithLabel(driver1, chatTextBoxLabel);
+    await fillTextInput(driver1, chatInput1, msg);
+    await click(await findButtonContainingText(driver1, "Send"));
+    await driver1.wait(
+      until.elementLocated(By.xpath(`//p[normalize-space()='You: ${msg}']`)),
+      3000
+    );
+    await driver2.wait(
+      until.elementLocated(By.xpath(`//p[normalize-space()='Partner: ${msg}']`)),
+      3000
+    );
+  });
+});
