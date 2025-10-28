95445d5dc5eaedefc7bb076e254aa060bee38109
Wei Guo (NCS) <wei.guo@ncs.com.sg>
2025-10-28 14:11:31 +0800

    basic layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,37 @@
-import Header from './components/Header/Header';
-import { Container } from '@mantine/core';
+import Header from "./components/Header/Header";
+import { Container, TextInput } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { useState } from "react";
 
 function App() {
+  const [tagList, setTags] = useState<string[]>([]);
+
+  const addTag = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key == 'Enter') {
+      setTags([...tagList, event.currentTarget.value])
+    }
+  };
+
   return (
-    <>
-      <Header/>
-      <Container>this is a test</Container>
-    </>
-  )
+    <div>
+      <Header />
+      <Container>
+        <TextInput
+          label="enter tag"
+          required
+          onKeyDown={addTag}
+        />
+        <>
+          {tagList.map((value: string) => (
+            <div>{value}</div>
+          ))}
+        </>
+        Click to Queue
+        <br />
+        <button>Queue</button>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,10 +1,44 @@
-function Header() {
-  return (
-    <div>
-      Header Here
-    </div>
-  )
+import { Burger, Container, Group } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
+import classes from "./header.module.css"
 
+const links = [
+  { link: '/about', label: 'Features' },
+  { link: '/pricing', label: 'Pricing' },
+  { link: '/learn', label: 'Learn' },
+  { link: '/community', label: 'Community' },
+];
+
+function Header() {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      data-active={active === link.link || undefined}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </a>
+  ));
+
+  return (
+    <header className={classes.header}>
+      <Container size="md" className={classes.inner}>
+        <Group gap={5} visibleFrom="xs" className={classes.group}>
+          {items}
+        </Group>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+      </Container>
+    </header>
+  );
 }
 
 export default Header
